d12b9c808d45a3c31e79a8931971ca8a28b41a9e
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-03 18:18:50 +0800

    feat: add input for search and dropdown for filter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,18 +10,34 @@ function HomePage() {
 
     return (
         <>
-            <div className={ isDarkMode ? "countries-container dark p-5" : "countries-container light p-5" }>
-                {
-                    countries.map((country, index) => (
-                        <Cards key={index} 
-                            image={country.flag} 
-                            name={country.name}
-                            population={country.population}
-                            region={country.region}
-                            capital={country.capital}
-                        />
-                    ))
-                }
+            <div className={ isDarkMode ? "dark" : "light" }> 
+                <div className="input-dropdown">
+                    <div className="input-group">
+                        <button className={ isDarkMode ? "elements-dark" : "elements-light" } type="button" id="button-addon1"><i className="fa-solid fa-magnifying-glass"></i></button>
+                        <input className={ isDarkMode ? "elements-dark" : "elements-light" } type="text"  placeholder="Search for a country..." aria-describedby="button-addon1" />
+                    </div>
+                    <div className="dropdown">
+                        <button className={ isDarkMode ? "dropdown-toggle elements-dark" : "dropdown-toggle elements-light" } type="button" data-bs-toggle="dropdown" aria-expanded="false">Filter by Region</button>
+                        <ul className={ isDarkMode ? "dropdown-menu elements-dark" : "dropdown-menu elements-light" }>
+                            <li><a className={ isDarkMode ? "dropdown-item elements-dark" : "dropdown-item elements-light" } href="#">Action</a></li>
+                            <li><a className={ isDarkMode ? "dropdown-item elements-dark" : "dropdown-item elements-light" } href="#">Another action</a></li>
+                            <li><a className={ isDarkMode ? "dropdown-item elements-dark" : "dropdown-item elements-light" } href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>    
+                <div className="countries-container">
+                    {
+                        countries.map((country, index) => (
+                            <Cards key={index} 
+                                image={country.flag} 
+                                name={country.name}
+                                population={country.population}
+                                region={country.region}
+                                capital={country.capital}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </>
     )

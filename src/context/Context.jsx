a491b56e6be29d1001708a5f126f1c913c6211ec
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-04 09:39:52 +0800

    refactor: add country details to context

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,11 +4,13 @@ export const Context = createContext();
 export const  ContextProvider = (props) => {
 
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [countryDetails, setCountryDetails] = useState([]);
     
     return (
         <>
             <Context.Provider value={{
-                isDarkMode, setIsDarkMode
+                isDarkMode, setIsDarkMode,
+                countryDetails, setCountryDetails,
             }}>
                 {props.children}
             </Context.Provider>

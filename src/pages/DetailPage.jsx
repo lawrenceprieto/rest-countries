02162082bdbba6414466c5d48b3bc11b142339d9
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-04 22:20:55 +0800

    refactor: add function to borders and back button

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -5,24 +5,35 @@ import data from "../assets/data.json";
 
 function DetailPage() {
 
-    const { isDarkMode, countryDetails } = useContext(Context);
+    const { isDarkMode, countryDetails , setCountryDetails } = useContext(Context);
     const navigate = useNavigate();
-    function handleBackButton() {
-        navigate("/");
-    }
     
+    let listBorders = [];
     const borders = countryDetails.borders;
-    const listBorders = [];
     if (borders) {
         for ( let i=0 ; i<borders.length ; i++) {
-            data.filter((alphaCode) => {
-                if (alphaCode.alpha3Code === borders[i]) {
-                    listBorders.push(alphaCode.translations.br);
+            data.filter((country) => {
+                if (country.alpha3Code === borders[i]) {
+                    listBorders.push(country);
                 } 
             });
         }
     } else {
         null;
+    }
+
+    function handleBackButton() {
+        navigate("/");
+    }
+
+    function handleBorderCountries(border) {
+         window.scrollTo({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+        });
+
+        setCountryDetails(border);
     }
 
     return (
@@ -43,7 +54,7 @@ function DetailPage() {
                             </div>
                             <div>
                                 <div className="mb-2" style={{fontWeight: "600"}}>Top Level Domain: <span style={{fontWeight: "300"}}>{countryDetails.topLevelDomain}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Currencies: <span style={{fontWeight: "300"}}>{countryDetails.currencies.map(currency => currency.name)}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Currencies: <span style={{fontWeight: "300"}}>{countryDetails.currencies[0].name}</span></div>
                                 <div className="mb-2" style={{fontWeight: "600"}}>Languages: {countryDetails.languages.map((language, index) => (<span style={{fontWeight: "300"}} key={index}>{language.name}, </span>))}</div>
                             </div>
                         </div>
@@ -51,8 +62,8 @@ function DetailPage() {
                             <div className="mb-2" style={{fontWeight: "600"}}>Border Countries: </div>
                             <div className="bc-details">
                                 {
-                                    listBorders.map((listBorder, index) => (
-                                        <div className={ isDarkMode ? "elements-dark px-3 py-1" : "elements-light px-3 py-1" } style={{fontWeight: "300"}} key={index}>{listBorder}</div>
+                                    listBorders.map((border, index) => (
+                                        <div className={ isDarkMode ? "elements-dark px-3 py-1" : "elements-light px-3 py-1" } style={{fontWeight: "300", cursor: "pointer"}} key={index} onClick={() => handleBorderCountries(border)}>{border.name}</div>
                                     ))
                                 }
                             </div>

b7d37be3765b896aef602c69a5e781065803bd3a
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-04 22:23:13 +0800

    refactor: changed storing and mapping of data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,49 @@
 import { useContext, useState } from "react";
-import data from "../assets/data.json"
-import Cards from "../components/Cards";
 import { Context } from "../context/Context";
 import { useNavigate } from "react-router-dom";
+import Cards from "../components/Cards";
+import data from "../assets/data.json";
+// import axios from "axios";
 
 function HomePage() {
 
     const { isDarkMode, setCountryDetails } = useContext(Context);
-    const [ items , setItems ] = useState(data);
-    const menuItems = [...new Set(data.map((regions) => regions.region))];
+    const filterMenus = [...new Set(data.map((regions) => regions.region))];
+    const [newData, setNewData] = useState(data);
+    const navigate = useNavigate();
 
-    function handleMenuItem(menuItem) {
+    // useEffect(() => {
+    //     axios.get('https://restcountries.com/v3.1/all')
+    //     .then(response => {
+    //         setData(response.data);
+    //         setNewData(response.data);
+    //     }, error => { 
+    //         console.log(error); 
+    //     });
+    // },[setData]);
+
+    function handleMenuItem(dropdownMenu) {
         const newItem = data.filter((regions) => {
-            return regions.region === menuItem; 
-        });
-        setItems(newItem);
+            return regions.region === dropdownMenu; 
+        }); 
+        
+        setNewData(newItem);
     }
 
-    const navigate = useNavigate();
     function handleCountry(country) {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        window.scrollTo({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+        });
+        
         setCountryDetails(country);
         navigate("detail-page");
     }
 
     return (
         <>
-            <div className={ isDarkMode ? "dark" : "light" }> 
+            <div className={ isDarkMode ? "dark" : "light" } style={{minHeight: "100vh"}}> 
                 <div className="input-dropdown">
                     <div className="input-field">
                         <button className={ isDarkMode ? "elements-dark" : "elements-light" } type="button" id="button-addon1"><i className="fa-solid fa-magnifying-glass"></i></button>
@@ -36,8 +53,8 @@ function HomePage() {
                         <button className={ isDarkMode ? "dropdown-toggle elements-dark" : "dropdown-toggle elements-light" } type="button" data-bs-toggle="dropdown" aria-expanded="false">Filter by Region <span className="px-3"></span></button>
                         <ul className={ isDarkMode ? "dropdown-menu elements-dark" : "dropdown-menu elements-light" }>
                             {
-                                menuItems.map((menuItem, index) => (
-                                    <li key={index}><a className={ isDarkMode ? "dropdown-item elements-dark" : "dropdown-item elements-light" } onClick={() => handleMenuItem(menuItem)}>{menuItem}</a></li>
+                                filterMenus.map((dropdownMenu, index) => (
+                                    <li key={index} style={{cursor: "pointer"}}><a className={ isDarkMode ? "dropdown-item elements-dark" : "dropdown-item elements-light" } onClick={() => handleMenuItem(dropdownMenu)}>{dropdownMenu}</a></li>
                                 ))
                             }
                         </ul>
@@ -45,7 +62,7 @@ function HomePage() {
                 </div>    
                 <div className="countries-container">
                     {
-                        items.map((country, index) => (
+                        newData.map((country, index) => (
                             <Cards key={index} 
                                 image={country.flag} 
                                 name={country.name}

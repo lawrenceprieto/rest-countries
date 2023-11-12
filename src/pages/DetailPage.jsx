2a8ea2ad891c4545f71735742886e8d4cc0d9d74
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-12 20:25:56 +0800

    refactor: update details page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,30 +1,77 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Context } from "../context/Context"
-import { useNavigate } from "react-router-dom";
-import data from "../assets/data.json";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 function DetailPage() {
 
-    const { isDarkMode, countryDetails , setCountryDetails } = useContext(Context);
+    const { isDarkMode, data, setLoading } = useContext(Context);
+    const [country, setCountry] = useState([]);
+    const [borders, setBorders] = useState([]);
+    const {countryName} = useParams();
     const navigate = useNavigate();
-    
-    let listBorders = [];
-    const borders = countryDetails.borders;
-    if (borders) {
-        for ( let i=0 ; i<borders.length ; i++) {
-            data.filter((country) => {
-                if (country.alpha3Code === borders[i]) {
-                    listBorders.push(country);
-                } 
-            });
-        }
-    } else {
-        null;
-    }
 
+    
     function handleBackButton() {
         navigate("/");
     }
+
+    // useEffect(() => {
+        const fetchCountryDataByName = async (name) => {
+            try {
+                const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+                const countryData = response.data[0];
+                if (!countryData) {
+
+                    setLoading(true);
+                } else {
+
+                    // convert the currency to object
+                    const currencyArray = countryData.currencies ? Object.values(countryData.currencies) : null;
+                    const currencyValue = currencyArray[0]?.name ? currencyArray[0].name : "None";
+
+                    // conver the native name to object
+                    const nativeNameArray = countryData.name.nativeName ? Object.values(countryData.name.nativeName) : null;
+                    const nativeNameValue = nativeNameArray[0]?.common ? nativeNameArray[0].common : "None";
+
+                    // convert the language to object
+                    const languageArray = countryData.languages ? Object.values(countryData.languages) : null;
+                    const languageValue = languageArray ? languageArray.map(language => language) : "None";
+
+                    setCountry({
+                        flag: countryData.flags?.png,
+                        name: countryData.name.common,
+                        nativeName: nativeNameValue,
+                        population: countryData.population,
+                        region: countryData.region,
+                        subregion: countryData?.subregion,
+                        capital: countryData?.capital,
+                        tld: countryData?.tld,
+                        currency: currencyValue,
+                        language: languageValue,
+                        borders: countryData?.borders
+                    });
+
+                    // get all the borders using codes 
+                    const bordersName = countryData.borders.map(borderCode => {
+                        const border = data.find(b => b.cca3 === borderCode);
+                        return border;
+                    });
+
+                    setBorders(bordersName);                    
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.error("Error fetching country data:", error);
+            }
+        };
+
+    //     fetchCountryDataByName();
+    // }, [data]);
+
+    useEffect(() => {
+        fetchCountryDataByName(countryName);
+    },[data, countryName]);
 
     function handleBorderCountries(border) {
          window.scrollTo({
@@ -33,37 +80,37 @@ function DetailPage() {
             behavior: 'smooth'
         });
 
-        setCountryDetails(border);
+        fetchCountryDataByName(border.name.common);
     }
-
+    
     return (
         <>
             <div className={ isDarkMode ? "details-page dark" : "details-page light" } style={{minHeight: "100vh"}}>
                 <button className={ isDarkMode ? "elements-dark px-4 py-2 my-5" : "elements-light px-4 py-2 my-5" } type="button" onClick={handleBackButton}><i className="fa-solid fa-arrow-left-long" style={{paddingRight: "10px"}}></i> Back</button>
                 <div className="details-container">
-                    <img src={countryDetails.flag} alt={countryDetails.name} />
+                    <img src={country?.flag} alt={country?.name} />
                     <div className="details-content">
-                        <div className="my-4" style={{fontWeight: "800", fontSize: "20px"}}>{countryDetails.name}</div>
+                        <div className="my-4" style={{fontWeight: "800", fontSize: "20px"}}>{country?.name}</div>
                         <div className="details-about">
                             <div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Native Name: <span style={{fontWeight: "300", wordWrap: "wrap"}}>{countryDetails.nativeName}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Population: <span style={{fontWeight: "300"}}>{new Intl.NumberFormat().format(countryDetails.population)}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Region: <span style={{fontWeight: "300"}}>{countryDetails.region}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Sub Region: <span style={{fontWeight: "300"}}>{countryDetails.subregion}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Capital: <span style={{fontWeight: "300"}}>{countryDetails.capital}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Native Name: <span style={{fontWeight: "300", wordWrap: "wrap"}}>{country?.nativeName}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Population: <span style={{fontWeight: "300"}}>{new Intl.NumberFormat().format(country?.population)}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Region: <span style={{fontWeight: "300"}}>{country.region}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Sub Region: <span style={{fontWeight: "300"}}>{country.subregion}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Capital: <span style={{fontWeight: "300"}}>{country.capital}</span></div>
                             </div>
                             <div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Top Level Domain: <span style={{fontWeight: "300"}}>{countryDetails.topLevelDomain}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Currencies: <span style={{fontWeight: "300"}}>{countryDetails.currencies[0].name}</span></div>
-                                <div className="mb-2" style={{fontWeight: "600"}}>Languages: {countryDetails.languages.map((language, index) => (<span style={{fontWeight: "300"}} key={index}>{language.name}, </span>))}</div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Top Level Domain: <span style={{fontWeight: "300"}}>{country.tld}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Currencies: <span style={{fontWeight: "300"}}>{country.currency}</span></div>
+                                <div className="mb-2" style={{fontWeight: "600"}}>Languages: <span style={{fontWeight: "300"}}>{country.language?.join(', ')}</span></div>
                             </div>
                         </div>
                         <div className="mt-4 mb-5 border-countries">
                             <div className="mb-2" style={{fontWeight: "600"}}>Border Countries: </div>
                             <div className="bc-details">
                                 {
-                                    listBorders.map((border, index) => (
-                                        <div className={ isDarkMode ? "elements-dark px-3 py-1" : "elements-light px-3 py-1" } style={{fontWeight: "300", cursor: "pointer"}} key={index} onClick={() => handleBorderCountries(border)}>{border.name}</div>
+                                    borders.map((border, index) => (
+                                        <div className={ isDarkMode ? "elements-dark px-3 py-1" : "elements-light px-3 py-1" } style={{fontWeight: "300", cursor: "pointer"}} key={index} onClick={() => handleBorderCountries(border)}>{border?.name.common}</div>                                    
                                     ))
                                 }
                             </div>

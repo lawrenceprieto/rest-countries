546c764e8327c87fbe23d32eaccb7b6927574e51
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-11-16 19:41:26 +0800

    add a no result component

---
+++ src/components/NoResult.jsx
@@ -0,0 +1,12 @@
+function NoResult() {
+    return(
+        <>
+            <div className="text-center" >
+                <h1>404</h1>
+                <h2>No Results Found</h2>
+            </div>
+        </>
+    )
+}
+
+export default NoResult
